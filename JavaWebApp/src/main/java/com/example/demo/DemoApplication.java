9bb00588812fd441795b9d50376e83e35c9d234b
Hugor Stéphane TchanouMbianda <stillyfreezle@yahoo.fr>
2022-10-25 21:14:25 -0400

    fix

--- JavaWebApp/src/main/java/com/example/demo/DemoApplication.java
+++ JavaWebApp/src/main/java/com/example/demo/DemoApplication.java
@@ -9,9 +9,9 @@ public class DemoApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 
-	    string name = "mbianda"
+		String name = "mbianda";
 
-		system.out.print(name);
+		System.out.println(name);
 	}
 
 }
